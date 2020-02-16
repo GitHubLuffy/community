0d0d72331f5c988fef0a045eec37cb77f081d60d
luffy <1051031961@qq.com>
2020-02-16 18:35:43 +0800

    完成编辑功能对于用户的可见和不可见和退出登录

---
+++ src/main/java/life/majiang/community/service/UserService.java
@@ -0,0 +1,35 @@
+package life.majiang.community.service;
+
+import life.majiang.community.mapper.UserMapper;
+import life.majiang.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * @Auther: luffy
+ * @Date: 2020-02-16 04:39
+ * @Description:
+ */
+
+@Service
+public class UserService {
+    @Autowired
+    private UserMapper userMapper;
+
+    public void createOrUpdate(User user) {
+        User dbUser = userMapper.findByAccountId(user.getAccountId());
+        if(dbUser == null){
+            //插入
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+        }else{
+            //更新
+            dbUser.setGmtModified(System.currentTimeMillis());
+            dbUser.setAvatarUrl(user.getAvatarUrl());
+            dbUser.setName(user.getName());
+            dbUser.setToken(user.getToken());
+            userMapper.update(dbUser);
+        }
+    }
+}
